7306ffa443bfba8716d850ebfac27fcdc16bb099
Juan Latorre <juanlatorre@protonmail.com>
2018-01-25 00:34:31 -0300

    follow your dreams

---
+++ dreams/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1516851076,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/solido-sheet0.png",
+		"images/personaje-sheet0.png",
+		"images/pastito-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
